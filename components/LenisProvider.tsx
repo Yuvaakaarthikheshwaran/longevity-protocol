f0ad92b2423c51d87d002a823f731e99dc0633f8
axby <146718245+Yuvaakaarthikheshwaran@users.noreply.github.com>
2026-06-30 15:46:25 +0000

    feat: refine lenis scroll feel

--- components/LenisProvider.tsx
+++ components/LenisProvider.tsx
@@ -6,10 +6,16 @@ import Lenis from "lenis";
 export default function LenisProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      smoothWheel: true,
-      touchMultiplier: 2,
+      duration: 1.45,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 1.18,
+      touchMultiplier: 1.8,
+      syncTouch: true,
+      syncTouchLerp: 0.08,
+      touchInertiaExponent: 1.6,
     });
 
     function raf(time: number) {
